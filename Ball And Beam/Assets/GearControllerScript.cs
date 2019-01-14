f0525cbeb54a264f507b1e89577ff1727fcfc589
marta.rutkowska <marta.rutkowska@protonmail.com>
2019-01-14 14:43:11 +0100

    model and basic ui added

---
+++ Ball And Beam/Assets/GearControllerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearControllerScript : MonoBehaviour {
+
+    [SerializeField] private float gearPower=100;
+    [SerializeField] private float motileScrewPower = 10;
+    [SerializeField] private float armScrewPower = 10;
+    [SerializeField] private GameObject gearSide;
+    [SerializeField] private GameObject motileScrew;
+    [SerializeField] private GameObject armScrew;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GearController();
+	}
+
+    private void GearController()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            gearSide.transform.Rotate(new Vector3(0, 0, gearPower) , Time.deltaTime*gearPower, Space.World);
+            motileScrew.transform.Rotate(new Vector3(0, -motileScrewPower, 0)*Time.deltaTime, Space.Self);
+            armScrew.transform.Rotate(new Vector3(0, armScrewPower, 0)*Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            gearSide.transform.Rotate(new Vector3(0, 0, -gearPower), Time.deltaTime*gearPower, Space.World);
+            motileScrew.transform.Rotate(new Vector3(0, motileScrewPower, 0)*Time.deltaTime, Space.Self);
+            armScrew.transform.Rotate(new Vector3(0, -armScrewPower, 0) * Time.deltaTime, Space.Self);
+
+
+        }
+    }
+}
---
+++ Ball And Beam/Assets/PID_control_system.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PID_control_system : MonoBehaviour {
+
+    ///
+    /// P_ball = -m*g*d/L/(J/R^2+m)/s^2
+    /// 
+    /// m - masa piłki- np. 0.11 kg
+    /// R - średnica piłki- np. 0.015 m
+    /// d - przesunięcie ramienia od silniczka, u nas jest to 0
+    /// g - przyspieszenie ziemskie 9,81 m/s^2
+    /// L - długość poprzeczki- np. 2 metry
+    /// J - moment bezwładności piłeczki - 9.99e-6 kg.m^2
+    /// r - koordynaty piłeczki
+    /// 
+    /// 
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
