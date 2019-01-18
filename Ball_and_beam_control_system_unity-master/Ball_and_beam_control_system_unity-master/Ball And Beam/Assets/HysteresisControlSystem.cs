33603b118bd9b74254791ec9bf9c914c15b359a9
marta.rutkowska <marta.rutkowska@protonmail.com>
2019-01-18 01:10:11 +0100

    small bug fixes, UI fixed

---
+++ Ball_and_beam_control_system_unity-master/Ball_and_beam_control_system_unity-master/Ball And Beam/Assets/HysteresisControlSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HysteresisControlSystem : ControlSystem
+{
+
+    [SerializeField] private float switchFrequency = 0.5f;
+    [SerializeField] public float min_hystheresis { get; set; }
+    [SerializeField] public float max_hystheresis { get; set; }
+    private float previous_u;
+    private float switchTime = 0;
+
+    override protected float CalculateError()
+    {
+        r = ball.transform.position.x;
+        R_star = pointer.position.x;
+        e_arr[1] = R_star - r;
+        return e_arr[1];
+    }
+
+    override public float CalculateControl()
+    {
+        float error = CalculateError();
+
+        if (error < -min_hystheresis && Time.time - switchTime >= switchFrequency)
+        {
+            switchTime = Time.time;
+            previous_u = normalizedMaxValue;
+            return normalizedMaxValue;
+        }
+        else if (error > max_hystheresis && Time.time - switchTime >= switchFrequency)
+        {
+            switchTime = Time.time;
+            previous_u = normalizedMinValue;
+            return normalizedMinValue;
+        }
+        else
+        {
+            return previous_u;
+        }
+    }
+
+    override protected void InitilizeAuxiliaries()
+    {
+        ResetParameters();
+    }
+
+    public void ResetParameters()
+    {
+        min_hystheresis = 0;
+        max_hystheresis = 0;
+    }
+}
---
+++ Ball_and_beam_control_system_unity-master/Ball_and_beam_control_system_unity-master/Ball And Beam/Assets/TwoStepControlSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoStepControlSystem : ControlSystem
+{
+
+    [SerializeField] private float switchFrequency = 0.5f;
+    private float previuos_u;
+    private float switchTime = 0;
+
+
+    override protected float CalculateError()
+    {
+        r = ball.transform.position.x;
+        R_star = pointer.position.x;
+        e_arr[1] = R_star - r;
+
+        return e_arr[1];
+    }
+
+    override public float CalculateControl()
+    {
+        float error = CalculateError();
+        if (error < 0 && Time.time - switchTime >= switchFrequency)
+        {
+            switchTime = Time.time;
+            previuos_u = normalizedMaxValue;
+            return normalizedMaxValue;
+        }
+        else if (error > 0 && Time.time - switchTime >= switchFrequency)
+        {
+            switchTime = Time.time;
+            previuos_u = normalizedMinValue;
+            return normalizedMinValue;
+
+        }
+        else
+        {
+            return previuos_u;
+        }
+    }
+
+    override protected void InitilizeAuxiliaries()
+    {
+    }
+}
